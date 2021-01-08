89f4270c81fe51f4c71df29b0c8390f6b229bbc9
AntsmithCode <hey@antsmith.dev>
2021-01-07 22:41:47 -0500

    (Feature) Added Scale Options

--- root/app.js
+++ root/app.js
@@ -1,7 +1,7 @@
 function tempConvert() {
   let celsiusCalc = parseFloat(document.getElementById("degrees").value); //onchange Event can be applied as alternative
 
-  document.getElementById("getCelsius").innerHTML = Math.round(
+switch ()  document.getElementById("getCelsius").innerHTML = Math.round(
     celsiusCalc * (9 / 5) + 32
   );
 }
