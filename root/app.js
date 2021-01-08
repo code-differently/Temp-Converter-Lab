22a43ba0e3c88af774c1e10337daab917a944ae7
AntsmithCode <hey@antsmith.dev>
2021-01-07 23:28:55 -0500

    (APP) Added Switch Function Case Fahrenheit and Tested-Success

--- root/app.js
+++ root/app.js
@@ -1,7 +1,23 @@
 function tempConvert() {
-  let celsiusCalc = parseFloat(document.getElementById("degrees").value); //onchange Event can be applied as alternative
+  let scaleCalc = document.getElementById("selectScale").value; //Grabs the value of 
+  let inputNum = parseFloat(document.getElementById("inputValue").value);
 
-switch ()  document.getElementById("getCelsius").innerHTML = Math.round(
-    celsiusCalc * (9 / 5) + 32
-  );
+  switch (scaleCalc) {
+    case "Fahrenheit":
+      document.getElementById("getCelsius").innerHTML = Math.round(
+        inputNum * (9 / 5) + 32
+      );
+      break;
+    case "Celsius":
+
+      
+      );
+      break;
+    case "Kelvin":
+
+      
+      
+      break;
+    case "Rankine":
+  }
 }
